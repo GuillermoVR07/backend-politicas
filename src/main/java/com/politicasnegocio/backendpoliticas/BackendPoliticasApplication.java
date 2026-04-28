adb5063db5902aba54dd98b73be49d884b6044c0
Guillermo Vaca Roca <guillermo.vaca07@gmail.com>
2026-04-28 05:19:37 -0400

    first commit

---
+++ src/main/java/com/politicasnegocio/backendpoliticas/BackendPoliticasApplication.java
@@ -0,0 +1,13 @@
+package com.politicasnegocio.backendpoliticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPoliticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPoliticasApplication.class, args);
+	}
+
+}
